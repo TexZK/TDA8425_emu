c2454c0b9db503f47e90e5681792351c9f043df1
Andrea Zoppi <texzk@email.it>
2023-03-13 21:05:21 +0100

    Updated license

--- src/TDA8425_emu.h
+++ src/TDA8425_emu.h
@@ -1,7 +1,7 @@
 /*
 BSD 2-Clause License
 
-Copyright (c) 2020, Andrea Zoppi
+Copyright (c) 2020-2023, Andrea Zoppi
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -56,7 +56,7 @@ extern "C" {
 
 // ============================================================================
 
-#define TDA8425_VERSION "0.1.2"
+#define TDA8425_VERSION "0.1.3"
 
 char const* TDA8425_GetVersion(void);
 
